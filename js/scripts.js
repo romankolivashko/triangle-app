1e84350ebeebc6e04baac97af802bc5016806f0d
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-12 21:01:20 -0700

    business logic is in place

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
+// User interface logic:
+
+let sideA;
+let sideB;
+let sideC;
+
+let equal = "Equilateral";
+let isos = "Isosceles";
+let scal = "Scalene";
+
+// Business logic:
 $(document).ready(function() {
-  $(".content").click(function() {
-    $(".card-showing").toggle();
-    $(".card-hidden").toggle();
+  $("form#calculate").submit(function(event) {
+   event.preventDefault();
+   $(".card-hidden").show();
+    const sideA = parseInt($("input#sideA").val());
+    const sideB = parseInt($("input#sideB").val());
+    const sideC = parseInt($("input#sideC").val());
+
+    if (sideA === sideB && sideB === sideC) {
+      $("#equal").show();
+      $("#isos").hide();
+      $("#scal").hide();
+    } else if ( sideA === sideB || sideB === sideC || sideC === sideA ) {
+      $("#equal").hide();
+      $("#isos").show();
+      $("#scal").hide();
+    } else {
+      $("#equal").hide();
+      $("#isos").hide();
+      $("#scal").show();
+    }
+
+    // $("#output").show();
   });
 });
+
+
 
